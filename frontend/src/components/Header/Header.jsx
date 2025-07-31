36caecfe59134b31bceb569230ac0f2efe6da871
Mithsala123 <piyumisudusinghe02@gmail.com>
2025-08-01 00:00:05 +0530

    homepage

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import './Header.css'
-import homeImg from '../../assets/new.jpg'
-
+import newImg from '../../assets/new.jpg'
 
 const Header = () => {
   return (
-    <div className="header" style={{backgroundImage: `url(${homeImg})`,backgroundRepeat: 'no-repeat',
-    backgroundSize: 'contain'}}>
-
-    <div className="heager-contend">
-        <h2>  Are You Hungry?</h2>
-        <h1>Don't Wait </h1>
+    <div
+      className="header"
+      style={{
+        backgroundImage: `url(${newImg})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }}
+    >
+      {/* Remove this div if you only want the image */}
+      <div className="heager-contend">
+        <h2>Are You Hungry?</h2>
+        <h1>Don't Wait</h1>
         <h2>Let's start to order food now</h2>
-        <p> Enjoy your favorite meals delivered fresh and fast! From local flavors to international dishes, satisfy your hunger with just a few clicks.</p>
+        <p>Enjoy your favorite meals delivered fresh and fast! From local flavors to international dishes, satisfy your hunger with just a few clicks.</p>
         <button>View Menu</button>
-    </div>
-      
+      </div> 
     </div>
   )
 }
 
 export default Header
+
