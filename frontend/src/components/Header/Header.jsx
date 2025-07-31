c7d751c3a0ffc41a9af5d4fa53e7dfabc948d78f
Mithsala123 <piyumisudusinghe02@gmail.com>
2025-07-31 22:42:48 +0530

    home page

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Header.css'
+import homeImg from '../../assets/home.jpg'
 
 
 const Header = () => {
   return (
-    <div className="header"> 
+    <div className="header" style={{backgroundImage: `url(${homeImg})`,backgroundRepeat: 'no-repeat',
+    backgroundSize: 'contain'}}>
+
+    <div className="heager-contend">
+        <h2>  Are You Hungry?</h2>
+        <h1>Don't Wait </h1>
+        <h2>Let's start to order food now</h2>
+        <p> Enjoy your favorite meals delivered fresh and fast! From local flavors to international dishes, satisfy your hunger with just a few clicks.</p>
+        <button>View Menu</button>
+    </div>
       
     </div>
   )
