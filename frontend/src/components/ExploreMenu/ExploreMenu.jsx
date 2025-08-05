e64282a4c66f5b12a06e8913437a3e0c237046d2
Mithsala123 <piyumisudusinghe02@gmail.com>
2025-08-05 23:14:22 +0530

    menu

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import './ExploreMenu.css'
+import rice from '../../assets/rice.png'
+import ramen from '../../assets/ramen.jpg'
+import orange from '../../assets/orange.jpg'
+import ramen2 from '../../assets/ramen2.jpg'
+import sprite from '../../assets/sprite.jpeg'
 
 const ExploreMenu = () => {
   return (
-    <div>
-      
+    <div className="explore-menu" id="explore-menu">
+      <h1> explore our menu</h1>
+      <p className="explore-menu-text">Choose your favorite foods.</p>
+      <div className="explore-menu-items">
+        <div className="explore-menu-item">
+          <img src={rice} alt="Rice" />
+          <h2>Rice</h2>
+        </div>
+        <div className="explore-menu-item">
+          <img src={ramen} alt="Ramen" />
+          <h2>Ramen</h2>
+        </div>
+        <div className="explore-menu-item">
+          <img src={orange} alt="Orange" />
+          <h2>Orange</h2>
+        </div>
+        <div className="explore-menu-item">
+          <img src={ramen2} alt="Ramen 2" />
+          <h2>Ramen 2</h2>
+        </div>
+        <div className="explore-menu-item">
+          <img src={sprite} alt="Sprite" />
+          <h2>Sprite</h2>
+        </div>
+      </div>
     </div>
   )
 }
