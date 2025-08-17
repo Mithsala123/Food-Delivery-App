5be535d16f0236ad94097c14dd59d0c5631011c9
Mithsala123 <piyumisudusinghe02@gmail.com>
2025-08-17 22:41:30 +0530

    do changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import {Route, Routes} from 'react-router-dom'
 import Home from './pages/home/Home.jsx'
 import Cart from './pages/Cart/Cart.jsx'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
-
+import Menu from './pages/Menu/Menu.jsx'
 
 
 const App = () => {
@@ -15,7 +15,8 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='/PlaceOrder' element={<PlaceOrder />} />
-        
+        <Route path='/Menu' element={<Menu />} />
+
       </Routes>
       
     </div>
